dcc7ee73abbccf52bf68a13981a754a6ba95d8fe
SeniorIntern <tnikhil.official@gmail.com>
2024-04-08 16:58:10 +0545

    Integrate cloudinary. Handle single file upload

--- node/index.js
+++ node/index.js
@@ -1,22 +1,46 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
 app.use(cors());
 app.use(express.json()); // read json data
 app.use(express.urlencoded({ extended: true })); // read encoded data
-app.use(fileUpload());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  }),
+);
 
 app.get("/info", async (req, res) => {
   res.status(200).json("API is live.");
 });
 
 app.post("/foo", async (req, res) => {
+  let file = req.files.file;
+  const result = await cloudinary.uploader.upload(file.tempFilePath, {
+    folder: "users",
+  });
   console.log(req.body);
   console.log(req.files); // log files
 
-  res.status(200).json(req.body);
+  const details = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    result,
+  };
+
+  res.status(200).send(details);
 });
 
 const port = 3000;
